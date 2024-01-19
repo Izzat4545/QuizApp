bed81cb7c8a591abd93e7479753d6b50bda94c03
Izzat4545 <izzatsultanov77@gmail.com>
2024-01-19 17:53:16 +0500

    minor changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { LoginComponent } from './login/login.component';
+import { QuizComponent } from './quiz/quiz.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CategoryComponent },
+  { path: 'login', component: LoginComponent },
+  { path: ':category', component: QuizComponent },
+];
