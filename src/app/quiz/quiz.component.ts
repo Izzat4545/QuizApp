9004dc6863b11b3ba05a2515f08c83a8682280e1
Izzatbek <55664400+Izzat4545@users.noreply.github.com>
2024-01-23 12:19:56 +0500

    some chamges

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,39 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { FirebaseMethodsService } from '../firebase-methods.service';
 import { categoryId } from '../../types/response';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatRadioModule, MatRippleModule],
   templateUrl: './quiz.component.html',
 })
 export class QuizComponent implements OnInit {
   constructor(
-    private router: ActivatedRoute,
+    private router: Router,
     private firebase: FirebaseMethodsService
   ) {}
   id: string = '';
   quiz: categoryId = [];
   async getQuiz() {
     this.quiz = await this.firebase.readData('Quiz/' + this.id);
-    console.log(this.quiz);
   }
   ngOnInit(): void {
-    this.router.url.subscribe((segments) => {
-      // Get the last segment of the URL
-      const lastSegment = segments[segments.length - 1].path;
+    const currentPath = this.router.url;
+    this.id = parseInt(currentPath.match(/\d+/)?.[0] || '0', 10).toString();
 
-      // Extract the ID using a regular expression
-      const match = lastSegment.match(/-(\d+)$/);
-
-      // Check if there is a match and extract the ID
-      if (match) {
-        this.id = match[1];
-      }
-      this.getQuiz();
-    });
+    this.getQuiz();
   }
 }
