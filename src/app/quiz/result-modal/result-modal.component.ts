ec371ee756131ade890e3be8219c61d4df1b7ee4
Izzatbek <55664400+Izzat4545@users.noreply.github.com>
2024-01-23 20:00:38 +0500

    added test report

--- src/app/quiz/result-modal/result-modal.component.ts
+++ src/app/quiz/result-modal/result-modal.component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { statistics } from '../../../types/response';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-result-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './result-modal.component.html',
 })
-export class ResultModalComponent {
+export class ResultModalComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<ResultModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: statistics[]
   ) {}
+
+  correctAnswers: boolean[] = [];
+
+  ngOnInit(): void {
+    this.storeCorrectAnswers();
+  }
+
+  storeCorrectAnswers() {
+    this.data.map((value) => {
+      if (value.answer === value.selectedAnswer) {
+        this.correctAnswers.push(true);
+      }
+    });
+  }
 
   onCloseClick(): void {
     this.dialogRef.close();
